577de723cb2e686a5279bed4b72a3f5e3ae33499
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-08 22:51:28 +0400

    Разбор на семинаре

---
+++ zadacha01/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное число 
+//из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+using System; // делать не обязательно, подразумевается в современных версиях языка
+
+int number = new Random ().Next (10, 100); 
+// датчик случайных чисел от 10 до 99 включительно
+Console.WriteLine(number);
+
+int digit1 = number / 10; // digit = цифра, number = число
+int digit2 = number % 10;
+
+if (digit1 >= digit2)
+    Console.WriteLine(digit1);
+else 
+    Console.WriteLine(digit2);
