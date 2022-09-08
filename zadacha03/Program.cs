b926d1064c78ef89dacd5360148b06e3ff2999f1
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-09 00:11:30 +0400

    Задача с семинара

---
+++ zadacha03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход 
+//два числа и выводить, является ли второе число кратным 
+//первому. Если число 2 не кратно числу 1, 
+//то программа выводит остаток от деления.
+
+Console.WriteLine("Введите число A");
+int FirstNumber = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите число B");
+int SecondNumber = int.Parse(Console.ReadLine());
+
+int result = SecondNumber % FirstNumber;
+
+if (result == 0)
+Console.WriteLine($"Число {SecondNumber} кратно числу {FirstNumber}"); 
+else
+Console.WriteLine($"Число {SecondNumber} не кратно числу {FirstNumber}, остаток {result}");
