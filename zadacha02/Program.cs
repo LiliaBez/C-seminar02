e21621b9aea69ba713bba6dcacf7088f292e3255
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-08 23:20:47 +0400

    Задача с семинара

---
+++ zadacha02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное 
+// число и удаляет вторую цифру этого числа.
+
+int number = new Random ().Next (100, 1000); 
+// датчик случайных чисел от 100 до 999 включительно
+Console.WriteLine(number);
+
+int digit1 = number / 100; // digit = цифра, number = число
+int digit2 = number % 10;
+
+Console.Write(digit1);
+Console.Write(digit2);
